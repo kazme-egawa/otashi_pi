b536baa5adea80cab55df9bd660e675df398f6d3
kazme-egawa <arnerican.f0otboy@gmail.com>
2018-07-01 03:26:48 +0900

    add sheet_test

---
+++ sheet_test.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+import serial
+
+ser = serial.Serial("/dev/ttyS0", baudrate = 9600, timeout = 2)
+
+# Text Print
+ser.write("Thermal Printer Shield\r");
+ser.write("Text Printing.\r");
+ser.write("\r\r");  # Line Feed x 2
+
+# QRcode Print
+ser.write(0x1D)  # 0x1D
+ser.write(0x78) # 0x78
+ser.write(0x4C)  # 0x4C
+ser.write(0x04)  # 0x04
+ser.write("TEST")   # DATA
+ser.write("\r\r\r\r\r\r");  # Line Feed x 6
+
+ser.close()
