fe3984ad9886680008198f6fa8718da0a38434ce
kazme-egawa <arnerican.f0otboy@gmail.com>
2018-07-01 18:41:49 +0900

    beer

--- sheet_test.py
+++ sheet_test.py
@@ -25,7 +25,11 @@ ser.write(chr(0x00)) # 0x00 or 01
 
 ser.write("\r");  # Line Feed
 
+ser.write(chr(0x1B)) # 0x1B
+ser.write(chr(0x6C)) # 0x6C
+ser.write(chr(0x10)) # 0x00 - 0x2F
 ser.write("担:01\r")
+
 ser.write("-------------------------------\r")
 ser.write("大きい声　　　　　　　　数量 1\r\r\r")
 
@@ -39,7 +43,18 @@ ser.write(chr(0x00)) # 0x00 or 01
 
 
 ser.write("（内うるさい度 ）\r")
+
+ser.write(chr(0x1B)) # 0x1B
+ser.write(chr(0x68)) # 0x68
+ser.write(chr(0x00)) # 0x00 or 01 or 02 or 03
+ser.write(chr(0x1B)) # 0x1B
+ser.write(chr(0x6C)) # 0x6C
+ser.write(chr(0x0C)) # 0x00 - 0x2F
 ser.write("レシート No.012\r")
+ser.write(chr(0x1B)) # 0x1B
+ser.write(chr(0x68)) # 0x68
+ser.write(chr(0x01)) # 0x00 or 01 or 02 or 03
+
 ser.write("-------------------------------\r")
 ser.write("》》》 記事見てね！《《《\r")
 ser.write("記事はこちら！！\r")
@@ -47,7 +62,7 @@ ser.write("記事はこちら！！\r")
 ser.write(chr(0x1B)) # 0x1B
 ser.write(chr(0x68)) # 0x68
 ser.write(chr(0x00)) # 0x00 or 01 or 02 or 03
-ser.write("https://fabcross.jp/list/series/sorepi/\r")
+ser.write("\rhttps://fabcross.jp/list/series/sorepi/\r\r")
 ser.write(chr(0x1B)) # 0x1B
 ser.write(chr(0x68)) # 0x68
 ser.write(chr(0x01)) # 0x00 or 01 or 02 or 03
