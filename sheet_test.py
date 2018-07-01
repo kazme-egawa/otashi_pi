c4efbe1f9780bfa8785e411978a1e3c9240b0bdb
kazme-egawa <arnerican.f0otboy@gmail.com>
2018-07-01 18:46:12 +0900

    beer

--- sheet_test.py
+++ sheet_test.py
@@ -4,13 +4,13 @@ import serial
 
 ser = serial.Serial("/dev/ttyS0", baudrate = 9600, timeout = 2)
 
-ser.write(chr(0x1B)) # 0x1B
-ser.write(chr(0x68)) # 0x68
-ser.write(chr(0x00)) # 0x00 or 01 or 02 or 03
+ser.write(chr(0x12)) # 0x12
+ser.write(chr(0x53)) # 0x53
+ser.write(chr(0x01)) # 0x00 or 01
 ser.write("新しいものづくりがわかるメディア\r")
-ser.write(chr(0x1B)) # 0x1B
-ser.write(chr(0x68)) # 0x68
-ser.write(chr(0x01)) # 0x00 or 01 or 02 or 03
+ser.write(chr(0x12)) # 0x12
+ser.write(chr(0x53)) # 0x53
+ser.write(chr(0x00)) # 0x00 or 01
 
 ser.write(chr(0x1C)) # 0x1C
 ser.write(chr(0x57)) # 0x57
@@ -27,7 +27,7 @@ ser.write("\r");  # Line Feed
 
 ser.write(chr(0x1B)) # 0x1B
 ser.write(chr(0x6C)) # 0x6C
-ser.write(chr(0x10)) # 0x00 - 0x2F
+ser.write(chr(0x20)) # 0x00 - 0x2F
 ser.write("担:01\r")
 
 ser.write("-------------------------------\r")
@@ -47,13 +47,19 @@ ser.write("（内うるさい度 ）\r")
 ser.write(chr(0x1B)) # 0x1B
 ser.write(chr(0x68)) # 0x68
 ser.write(chr(0x00)) # 0x00 or 01 or 02 or 03
+ser.write(chr(0x12)) # 0x12
+ser.write(chr(0x53)) # 0x53
+ser.write(chr(0x01)) # 0x00 or 01
 ser.write(chr(0x1B)) # 0x1B
 ser.write(chr(0x6C)) # 0x6C
-ser.write(chr(0x0C)) # 0x00 - 0x2F
+ser.write(chr(0x1A)) # 0x00 - 0x2F
 ser.write("レシート No.012\r")
 ser.write(chr(0x1B)) # 0x1B
 ser.write(chr(0x68)) # 0x68
 ser.write(chr(0x01)) # 0x00 or 01 or 02 or 03
+ser.write(chr(0x12)) # 0x12
+ser.write(chr(0x53)) # 0x53
+ser.write(chr(0x00)) # 0x00 or 01
 
 ser.write("-------------------------------\r")
 ser.write("》》》 記事見てね！《《《\r")
@@ -67,7 +73,7 @@ ser.write(chr(0x1B)) # 0x1B
 ser.write(chr(0x68)) # 0x68
 ser.write(chr(0x01)) # 0x00 or 01 or 02 or 03
 
-ser.write("[それ、ラズパイでつくれるよ]で検索！\r")
+ser.write("[それ、ラズパイでつくれるよ]\rで検索！\r")
 ser.write("\r\r");  # Line Feed
 
 # QRcode Print
