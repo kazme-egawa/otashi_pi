f99375391a211e1bdfb24ee5aa297c6929d936bc
kazme-egawa <arnerican.f0otboy@gmail.com>
2018-07-01 18:13:00 +0900

    test beer

--- sheet_test.py
+++ sheet_test.py
@@ -4,17 +4,26 @@ import serial
 
 ser = serial.Serial("/dev/ttyS0", baudrate = 9600, timeout = 2)
 
-# Text Print
-ser.write("Thermal Printer Shield\r");
-ser.write("Text Printing.\r");
-ser.write("\r\r");  # Line Feed x 2
+ser.write("新しいものづくりがわかるメディア\r")
+ser.write("fabcross\r")
+ser.write("担:01\r")
+ser.write("ーーーーーーーーーーーーーーーーー\r")
+ser.write("大きい声　　　　　　　　数量 1\r\r\r")
+ser.write("内容\r\r\r\r\r\r")
+ser.write("（内うるさい度 ）\r")
+ser.write("レシート No.012\r")
+ser.write("ーーーーーーーーーーーーーーーーー\r")
+ser.write("》》》記事見てね！《《《\r")
+ser.write("記事はこちら！！\r")
+ser.write("https://fabcross.jp/list/series/sorepi/\r")
+ser.write("[それ、ラズパイでつくれるよ]で検索！\r")
 
 # QRcode Print
 ser.write(chr(0x1D))  # 0x1D
 ser.write(chr(0x78)) # 0x78
 ser.write(chr(0x4C))  # 0x4C
-ser.write(chr(0x04))  # 0x04
-ser.write("TEST")   # DATA
+ser.write(chr(0x27))  # 0x04
+ser.write("https://fabcross.jp/list/series/sorepi/")   # DATA
 ser.write("\r\r\r\r\r\r");  # Line Feed x 6
 
 ser.close()
